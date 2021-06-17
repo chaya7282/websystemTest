ac1ffade8464c2f79784336df01c31350aab384c
chayaZilb <chaya.zilberstein@gmail.com>
2021-06-17 18:21:26 +0300

    zzz

--- app.py
+++ app.py
@@ -18,18 +18,7 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationE
 from flask_wtf import FlaskForm
 
 
-app = Flask(__name__)
-SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-SQLALCHEMY_RECORD_QUERIES = True
-app.config['SECRET_KEY'] = 'secret'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://b8ff07330af1f1:c66beaa7@us-cdbr-east-04.cleardb.com/heroku_e7818c97bf7c572'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-#app.config['SQLALCHEMY_DATABASE_URI']='postgresql+psycopg2://postgres:chaya@localhost/chayadb'
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
 
 class ProductForm(FlaskForm):
 
