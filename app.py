16e8baf2294627cf56f10fd5c7f9496bbe9d735c
chayaZilb <chaya.zilberstein@gmail.com>
2021-06-17 18:40:29 +0300

    zzz

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextA
 from flask_wtf.file import FileField, FileAllowed,  FileRequired
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_wtf import FlaskForm
-
+from webTest import app,db
 
 
 
