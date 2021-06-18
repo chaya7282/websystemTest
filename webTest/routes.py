7e3eb3456756d0c6a9f02403cc6b3c9072bae29e
chayaZilb <chaya.zilberstein@gmail.com>
2021-06-18 15:02:51 +0300

    zzz

--- webTest/routes.py
+++ webTest/routes.py
@@ -4,7 +4,7 @@ from flask_wtf.file import FileField, FileAllowed,  FileRequired
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_wtf import FlaskForm
 from webTest import app,db
-from webTest.models import Product
+from webTest.models import Product, Store
 
 
 class ProductForm(FlaskForm):
@@ -27,8 +27,40 @@ def products():
     products = Product.query.all()
     print(len(products))
 
-    return render_template('test5.html',products=products)
+    return render_template('products.html',products=products)
 
+@app.route("/product/<int:product_id>/update", methods=['GET', 'POST'])
+
+def update_product_details(product_id):
+    product = Product.query.get_or_404(product_id)
+    form = ProductForm()
+    if form.validate_on_submit():
+
+        product.company = form.company.data
+        product.name = form.name.data
+        product.price = form.price.data
+        product.description= form.description.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('inventory_managment'))
+    elif request.method == 'GET':
+        form.name.data= '{element}'.format(element=product.name)
+        form.company.data='{element}'.format(element=product.company)
+        form.price.data = '{element}'.format(element=product.price)
+        form.description.data = '{element}'.format(element=product.description)
+
+        return render_template('add_product.html', title='Update Product',form=form, legend='Update')
+
+@app.route("/product/<int:product_id>/add_product_to_store", methods=['GET', 'POST'])
+def add_product_to_store(product_id):
+    product = Product.query.get_or_404(product_id)
+    store = Store.query.filter_by(product=product).first()
+    if not store:
+        store=Store(product=product)
+        db.session.add(store)
+        db.session.commit()
+        flash('Your item was added!', 'success')
+    return redirect(url_for('store'))
 
 
 
